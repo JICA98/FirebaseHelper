ad46ed60de7d9847b630cc8db04f27f0372d8705
Jishnu Raj <jishnu.pooradam@hotmail.com>
2020-11-30 10:28:05 +0530

    Updated

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/FirebaseHelper/FirebaseHelperBPLibrary.generated.h
@@ -0,0 +1,126 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+ 
+#ifdef FIREBASEHELPER_FirebaseHelperBPLibrary_generated_h
+#error "FirebaseHelperBPLibrary.generated.h already included, missing '#pragma once' in FirebaseHelperBPLibrary.h"
+#endif
+#define FIREBASEHELPER_FirebaseHelperBPLibrary_generated_h
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_SPARSE_DATA
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execMakeParentChildJson); \
+	DECLARE_FUNCTION(execJsonStringFromJsonString); \
+	DECLARE_FUNCTION(execJsonFieldFromIntegerArray); \
+	DECLARE_FUNCTION(execJsonFieldFromNumberArray); \
+	DECLARE_FUNCTION(execJsonFieldFromStringArray); \
+	DECLARE_FUNCTION(execJsonFieldFromBoolArray); \
+	DECLARE_FUNCTION(execJsonStringFromBool); \
+	DECLARE_FUNCTION(execJsonStringFromString); \
+	DECLARE_FUNCTION(execJsonStringFromNumber); \
+	DECLARE_FUNCTION(execJsonStringFromInteger); \
+	DECLARE_FUNCTION(execGetAuthID); \
+	DECLARE_FUNCTION(execSetupFirebase);
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execMakeParentChildJson); \
+	DECLARE_FUNCTION(execJsonStringFromJsonString); \
+	DECLARE_FUNCTION(execJsonFieldFromIntegerArray); \
+	DECLARE_FUNCTION(execJsonFieldFromNumberArray); \
+	DECLARE_FUNCTION(execJsonFieldFromStringArray); \
+	DECLARE_FUNCTION(execJsonFieldFromBoolArray); \
+	DECLARE_FUNCTION(execJsonStringFromBool); \
+	DECLARE_FUNCTION(execJsonStringFromString); \
+	DECLARE_FUNCTION(execJsonStringFromNumber); \
+	DECLARE_FUNCTION(execJsonStringFromInteger); \
+	DECLARE_FUNCTION(execGetAuthID); \
+	DECLARE_FUNCTION(execSetupFirebase);
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUFirebaseHelperBPLibrary(); \
+	friend struct Z_Construct_UClass_UFirebaseHelperBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UFirebaseHelperBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/FirebaseHelper"), NO_API) \
+	DECLARE_SERIALIZER(UFirebaseHelperBPLibrary)
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUFirebaseHelperBPLibrary(); \
+	friend struct Z_Construct_UClass_UFirebaseHelperBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UFirebaseHelperBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/FirebaseHelper"), NO_API) \
+	DECLARE_SERIALIZER(UFirebaseHelperBPLibrary)
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFirebaseHelperBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFirebaseHelperBPLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFirebaseHelperBPLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFirebaseHelperBPLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFirebaseHelperBPLibrary(UFirebaseHelperBPLibrary&&); \
+	NO_API UFirebaseHelperBPLibrary(const UFirebaseHelperBPLibrary&); \
+public:
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFirebaseHelperBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFirebaseHelperBPLibrary(UFirebaseHelperBPLibrary&&); \
+	NO_API UFirebaseHelperBPLibrary(const UFirebaseHelperBPLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFirebaseHelperBPLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFirebaseHelperBPLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFirebaseHelperBPLibrary)
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_PRIVATE_PROPERTY_OFFSET
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_12_PROLOG
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_PRIVATE_PROPERTY_OFFSET \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_SPARSE_DATA \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_RPC_WRAPPERS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_INCLASS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_PRIVATE_PROPERTY_OFFSET \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_SPARSE_DATA \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_INCLASS_NO_PURE_DECLS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h_15_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class FirebaseHelperBPLibrary."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FIREBASEHELPER_API UClass* StaticClass<class UFirebaseHelperBPLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirebaseHelperBPLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/FirebaseHelper/FirestoreValue.generated.h
@@ -0,0 +1,138 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+struct FJsonValueB;
+ struct FJsonValueB;
+struct FGeoPoint;
+#ifdef FIREBASEHELPER_FirestoreValue_generated_h
+#error "FirestoreValue.generated.h already included, missing '#pragma once' in FirestoreValue.h"
+#endif
+#define FIREBASEHELPER_FirestoreValue_generated_h
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_38_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FJsonValueB_Statics; \
+	FIREBASEHELPER_API static class UScriptStruct* StaticStruct();
+
+
+template<> FIREBASEHELPER_API UScriptStruct* StaticStruct<struct FJsonValueB>();
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_15_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FGeoPoint_Statics; \
+	FIREBASEHELPER_API static class UScriptStruct* StaticStruct();
+
+
+template<> FIREBASEHELPER_API UScriptStruct* StaticStruct<struct FGeoPoint>();
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_SPARSE_DATA
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execMapValue); \
+	DECLARE_FUNCTION(execArrayValue); \
+	DECLARE_FUNCTION(execGeoPointValue); \
+	DECLARE_FUNCTION(execReferenceValue); \
+	DECLARE_FUNCTION(execBytesValue); \
+	DECLARE_FUNCTION(execStringValue); \
+	DECLARE_FUNCTION(execTimestampValue); \
+	DECLARE_FUNCTION(execFloatValue); \
+	DECLARE_FUNCTION(execIntegerValue); \
+	DECLARE_FUNCTION(execBooleanValue);
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execMapValue); \
+	DECLARE_FUNCTION(execArrayValue); \
+	DECLARE_FUNCTION(execGeoPointValue); \
+	DECLARE_FUNCTION(execReferenceValue); \
+	DECLARE_FUNCTION(execBytesValue); \
+	DECLARE_FUNCTION(execStringValue); \
+	DECLARE_FUNCTION(execTimestampValue); \
+	DECLARE_FUNCTION(execFloatValue); \
+	DECLARE_FUNCTION(execIntegerValue); \
+	DECLARE_FUNCTION(execBooleanValue);
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUFirestoreValue(); \
+	friend struct Z_Construct_UClass_UFirestoreValue_Statics; \
+public: \
+	DECLARE_CLASS(UFirestoreValue, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/FirebaseHelper"), NO_API) \
+	DECLARE_SERIALIZER(UFirestoreValue)
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_INCLASS \
+private: \
+	static void StaticRegisterNativesUFirestoreValue(); \
+	friend struct Z_Construct_UClass_UFirestoreValue_Statics; \
+public: \
+	DECLARE_CLASS(UFirestoreValue, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/FirebaseHelper"), NO_API) \
+	DECLARE_SERIALIZER(UFirestoreValue)
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFirestoreValue(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFirestoreValue) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFirestoreValue); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFirestoreValue); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFirestoreValue(UFirestoreValue&&); \
+	NO_API UFirestoreValue(const UFirestoreValue&); \
+public:
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFirestoreValue(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFirestoreValue(UFirestoreValue&&); \
+	NO_API UFirestoreValue(const UFirestoreValue&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFirestoreValue); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFirestoreValue); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFirestoreValue)
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_PRIVATE_PROPERTY_OFFSET
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_58_PROLOG
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_PRIVATE_PROPERTY_OFFSET \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_SPARSE_DATA \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_RPC_WRAPPERS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_INCLASS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_PRIVATE_PROPERTY_OFFSET \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_SPARSE_DATA \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_RPC_WRAPPERS_NO_PURE_DECLS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_INCLASS_NO_PURE_DECLS \
+	SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h_61_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class FirestoreValue."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FIREBASEHELPER_API UClass* StaticClass<class UFirestoreValue>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SimpleProject_Plugins_FirebaseHelper_Source_FirebaseHelper_Public_FirestoreValue_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
